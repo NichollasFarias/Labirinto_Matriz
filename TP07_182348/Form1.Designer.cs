8bed0e4895a5aab2f8e18fb7c20c5cfe88386b1d
NichollasFarias <ni.chollas@outlook.com>
2023-09-28 17:47:10 -0300

    Enviando

---
+++ TP07_182348/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TP07_182348
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pb_player = new System.Windows.Forms.PictureBox();
+            this.LblTempo = new System.Windows.Forms.Label();
+            this.LblTexto = new System.Windows.Forms.Label();
+            this.Tempo = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImage = global::TP07_182348.Properties.Resources.Labirinto;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(71, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(799, 538);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pb_player
+            // 
+            this.pb_player.BackColor = System.Drawing.Color.White;
+            this.pb_player.Image = ((System.Drawing.Image)(resources.GetObject("pb_player.Image")));
+            this.pb_player.Location = new System.Drawing.Point(798, 23);
+            this.pb_player.Name = "pb_player";
+            this.pb_player.Size = new System.Drawing.Size(37, 48);
+            this.pb_player.TabIndex = 2;
+            this.pb_player.TabStop = false;
+            // 
+            // LblTempo
+            // 
+            this.LblTempo.AutoSize = true;
+            this.LblTempo.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTempo.Location = new System.Drawing.Point(987, 23);
+            this.LblTempo.Name = "LblTempo";
+            this.LblTempo.Size = new System.Drawing.Size(85, 32);
+            this.LblTempo.TabIndex = 3;
+            this.LblTempo.Text = "00:00";
+            // 
+            // LblTexto
+            // 
+            this.LblTexto.AutoSize = true;
+            this.LblTexto.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTexto.Location = new System.Drawing.Point(876, 23);
+            this.LblTexto.Name = "LblTexto";
+            this.LblTexto.Size = new System.Drawing.Size(116, 32);
+            this.LblTexto.TabIndex = 3;
+            this.LblTexto.Text = "Tempo:";
+            // 
+            // Tempo
+            // 
+            this.Tempo.Enabled = true;
+            this.Tempo.Tick += new System.EventHandler(this.Tempo_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1084, 562);
+            this.Controls.Add(this.LblTexto);
+            this.Controls.Add(this.LblTempo);
+            this.Controls.Add(this.pb_player);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MineLab";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pb_player;
+        private System.Windows.Forms.Label LblTempo;
+        private System.Windows.Forms.Label LblTexto;
+        private System.Windows.Forms.Timer Tempo;
+    }
+}
+
---
+++ TP07_182348/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TP07_182348
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        #region Variavel Global
+
+        static string[,] Lab = new string[24, 36]
+            {
+            {   "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    },
+            {   "1",    "1",    "1",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "1",    },
+            {   "1",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    },
+            {   "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "0",    "0",    "0",    "0",    "0",    "1",    "0",    "1",    "1",    "0",    "0",    "0",    "1",    "0",    "1",    "1",    "0",    "0",    "0",    "0",    "0",    "1",    "0",    "1",    "1",    },
+            {   "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "0",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "0",    "1",    "1",    "1",    "0",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "0",    "1",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "0",    "0",    "0",    "0",    "1",    "1",    "0",    "0",    "0",    "0",    "0",    "1",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    },
+            {   "1",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    },
+            {   "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "0",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    },
+            {   "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "0",    "1",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "1",    "0",    "1",    "0",    "0",    "0",    "0",    "0",    "1",    "1",    },
+            {   "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "0",    "1",    "1",    },
+            {   "1",    "1",    "0",    "0",    "0",    "1",    "0",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    },
+            {   "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    },
+            {   "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "0",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "1",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "0",    "0",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "0",    "1",    "0",    "1",    "0",    "1",    "1",    "1",    "1",    },
+            {   "1",    "1",    "0",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "1",    "0",    "1",    "1",    "1",    "1",    },
+
+            };
+        int posLinha = 0, posColuna = 35;
+        string controle;
+
+        #endregion
+
+        #region movimentação
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int x = pb_player.Location.X;
+            int y = pb_player.Location.Y;
+
+            if ((x == 798) && (y == 483))
+            {
+                timer1.Stop();
+                MessageBox.Show("Você Ganhou", "Fim de Jogo");
+                //Reiniciar();
+            }
+            else if (controle == "A")
+            {
+                pb_player.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Esquerda.png");
+                pb_player.Size = new Size(37, 48);
+
+                if (posColuna == 0)
+                {
+                    return;
+                }
+                if (Lab[posLinha, posColuna - 1] == "1")
+                {
+                    pb_player.Location = new Point(x - 20, y);
+                    posColuna--;
+                }
+            }
+            else if (controle == "D")
+            {
+                pb_player.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Direita.png");
+                pb_player.Size = new Size(37, 48);
+
+                if (posColuna == 35)
+                {
+                    return;
+                }
+                if (Lab[posLinha, posColuna + 1] == "1")
+                {
+                    pb_player.Location = new Point(x + 20, y);
+                    posColuna++;
+                }
+            }
+            else if (controle == "S")
+            {
+                pb_player.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Frente.png");
+                pb_player.Size = new Size(37, 48);
+
+                if (posLinha == 23)
+                {
+                    return;
+                }
+                if (Lab[posLinha + 1, posColuna] == "1")
+                {
+                    pb_player.Location = new Point(x, y + 20);
+                    posLinha++;
+                }
+            }
+            else if (controle == "W")
+            {
+                pb_player.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Frente.png");
+                pb_player.Size = new Size(37, 48);
+
+                if (posLinha == 0)
+                {
+                    return;
+                }
+                if (Lab[posLinha - 1, posColuna] == "1")
+                {
+                    pb_player.Location = new Point(x, y - 20);
+                    posLinha--;
+                }
+            }
+            timer1.Stop();
+        }
+        #endregion
+
+        #region Teclas
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString().ToUpper() == "W" || e.KeyChar.ToString().ToUpper() == "S" || e.KeyChar.ToString().ToUpper() == "A" || e.KeyChar.ToString().ToUpper() == "D")
+            {
+                controle = e.KeyChar.ToString().ToUpper();
+                timer1.Start();
+            }
+            else
+            {
+                e.KeyChar = Convert.ToChar(0);
+            }
+        }
+
+
+        #endregion
+
+        //#region Reniciar
+        //public void Reiniciar()
+        //{
+        //    if (MessageBox.Show("Você gostaria de jogar novamente?", "Reiniciar", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+        //    {
+              
+        //    }
+
+        //}
+        //#endregion
+
+        #region Sair
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Você gostaria de fechar?", "Deseja Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+        #endregion
+
+        #region Tempo
+        private void Tempo_Tick(object sender, EventArgs e)
+        {
+            int segundos = 10;
+            LblTempo.Text = segundos.ToString();
+            segundos--;
+        }
+        #endregion
+    }
+}
+
+
+
+
+
